d1522d37829db151c7fbabf0591a904d47a7d087
Bohdan Marko <goldenbohdanm@gmail.com>
2024-09-26 18:34:51 +0300

    Add a logout functionality

--- user/views.py
+++ user/views.py
@@ -1,5 +1,8 @@
-from rest_framework import generics
+from django.contrib.auth import logout
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from user.serializers import UserSerializer
@@ -16,3 +19,15 @@ class ManageUserView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        logout(request)
+
+        return Response(
+            {"Success": "You have successfully logged out"},
+            status=status.HTTP_200_OK,
+        )
